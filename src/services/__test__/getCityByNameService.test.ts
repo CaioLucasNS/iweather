55237f2388460d47b86d510f6a79875ed42f161f
Caio Lucas <caio.lucas.do.silva@accenture.com>
2024-02-28 23:34:20 -0300

    organizando pasta para mocks

--- src/services/__test__/getCityByNameService.test.ts
+++ src/services/__test__/getCityByNameService.test.ts
@@ -1,29 +1,15 @@
 import { api } from "../api";
-import {
-  CityAPIResponse,
-  getCityByNameService,
-} from "@services/getCityByNameService";
+import { getCityByNameService } from "@services/getCityByNameService";
+import { mockCityAPIResponse } from "@__tests__/mocks/mockCityAPIResopnse";
 
 describe("API: getCityByNameService", () => {
   it("should return city details", async () => {
-    const data: CityAPIResponse = {
-      id: "1",
-      name: "São Paulo",
-      sys: {
-        country: "BR",
-      },
-      coord: {
-        lat: 123,
-        lon: 456,
-      },
-    };
     // spyOn -> fica observando as requests que serão feitas pra API
     // quando acontecer uma request do tipo GET, será interceptada
     // então, o retorno será mockado
-    jest.spyOn(api, "get").mockResolvedValue({ data });
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse });
     const response = await getCityByNameService("São Paulo");
 
-    // toBeGreaterThan -> maior do que
     expect(response.length).toBeGreaterThan(0);
   });
 });
