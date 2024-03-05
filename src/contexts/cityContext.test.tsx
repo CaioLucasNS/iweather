34cddeec4abcb2b2c622e2c074f58320f548f50e
Caio Lucas <caio.lucas.do.silva@accenture.com>
2024-03-05 17:22:39 -0300

    waitFor e act

--- src/contexts/cityContext.test.tsx
+++ src/contexts/cityContext.test.tsx
@@ -1,11 +1,18 @@
-import { renderHook } from "@testing-library/react-native";
+import { act, renderHook, waitFor } from "@testing-library/react-native";
 import { useCity } from "@hooks/useCity";
 import { CityProvider } from "./CityContext";
 
 describe("Context: CityContext", () => {
-  it("should be change selected city", () => {
+  it("should be change selected city", async () => {
     // É necessário passar o provider através do segundo param do renderHook com o wrapper
     const { result } = renderHook(() => useCity(), { wrapper: CityProvider });
-    console.log("result: ", result);
+
+    // waitFor = aguarda a solicitação assincrona que tem dentro do context
+    // act = lidar com a atualização de estado que acontece dentro do context
+    await waitFor(() =>
+      act(() => {
+        console.log("result: ", result.current);
+      })
+    );
   });
 });
